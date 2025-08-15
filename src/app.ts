6839bab9a4bec66a0c9f564ab4df2fcf45330e2d
Chris Vouga <crvouga@gmail.com>
2025-08-15 01:04:56 -0700

    Enable collisions and gravity for camera and ground in the Babylon.js scene. Refactor ground creation to improve size and material properties, while removing unused sphere logic.

--- src/app.ts
+++ src/app.ts
@@ -32,12 +32,16 @@ export class App {
 
 const createScene = (engine: BABYLON.Engine, canvas: HTMLCanvasElement) => {
   const scene = new BABYLON.Scene(engine);
+  scene.collisionsEnabled = true;
 
   const camera = new BABYLON.FreeCamera(
     "camera1",
     new BABYLON.Vector3(0, 5, -10),
     scene
   );
+  camera.checkCollisions = true;
+  camera.applyGravity = true;
+  camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
 
   camera.setTarget(BABYLON.Vector3.Zero());
 
@@ -50,46 +54,21 @@ const createScene = (engine: BABYLON.Engine, canvas: HTMLCanvasElement) => {
   );
 
   light.intensity = 0.7;
+  createGround(scene);
 
-  const sphere = BABYLON.MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 2, segments: 32 },
-    scene
-  );
-  let startPos = 2;
-  sphere.position.y = startPos;
+  return scene;
+};
 
+const createGround = (scene: BABYLON.Scene) => {
   const ground = BABYLON.MeshBuilder.CreateGround(
     "ground",
-    { width: 6, height: 6 },
+    { width: 100, height: 100 },
     scene
   );
+  ground.checkCollisions = true;
   const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
   groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.8, 0.5);
   ground.material = groundMaterial;
   groundMaterial.bumpTexture = new BABYLON.Texture("./normal.jpg", scene);
-
-  const redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
-  redMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
-  sphere.material = redMaterial;
-
-  let sphereVelocity = 0;
-  const gravity = 0.009;
-  const reboundLoss = 0.1;
-
-  scene.registerBeforeRender(() => {
-    sphereVelocity += gravity;
-    let newY = sphere.position.y - sphereVelocity;
-    sphere.position.y -= sphereVelocity;
-    if (newY < 1) {
-      sphereVelocity = (reboundLoss - 1) * sphereVelocity;
-      newY = 1;
-    }
-    sphere.position.y = newY;
-    if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
-      sphere.position.y = startPos++;
-    }
-  });
-
-  return scene;
+  return ground;
 };
